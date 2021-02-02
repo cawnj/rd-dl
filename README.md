97341963eda423516cb9c134533f6f83b98b255a
Conor Joyce <conor.joyce27@mail.dcu.ie>
2021-02-02 03:18:42 +0000

    done

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rd-dl
 A quick and dirty CLI downloader for Real-Debrid
+
+## Usage
+
+Input your API Key from https://real-debrid.com/apitoken in the "API_KEY" variable inside the script  
+Install requirements with `pip3 install -r requirements.txt`
+
+Run `python3 rd-dl.py <url>` or `python3 rd-dl.py <.txt file containing urls>`
---
+++ rd-dl.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+import urllib
+import wget
+
+# static
+API_KEY = ""
+API_ENDPOINT_LINK = "https://api.real-debrid.com/rest/1.0/unrestrict/link"
+API_ENDPOINT_FOLDER = "https://api.real-debrid.com/rest/1.0/unrestrict/folder"
+HEADERS = {'Authorization': "Bearer " + API_KEY}
+
+# getting inputs
+arg=sys.argv[1]
+if '.txt' in arg:
+	try:
+		with open(arg) as f:
+			links = f.readlines()
+		links = [x.strip() for x in links]
+	except FileNotFoundError:
+		sys.exit("Error: file not found")
+else:
+	links = [arg]
+
+# converting folder to files if needed
+new_links=[]
+for link in links:
+	if 'folder' in link:
+		response = requests.post(url=API_ENDPOINT_FOLDER, headers=HEADERS, data={'link': link})
+		jsonResponse = response.json()
+		for file in jsonResponse:
+			new_links.append(file)
+	else:
+		new_links.append(link)
+
+# download all files
+for link in new_links:
+	print('Getting link from real-debrid...')
+	response = requests.post(url=API_ENDPOINT_LINK, headers=HEADERS, data={'link': link})
+	jsonResponse = response.json()
+	try:
+		downloadUrl = jsonResponse['download']
+	except KeyError:
+		sys.exit("Error: " + jsonResponse['error'])
+	downloadUrlunq = urllib.parse.unquote(downloadUrl)
+	print('Downloading with wget...')
+	wget.download(downloadUrlunq)
+	print()
+print('Success!')
